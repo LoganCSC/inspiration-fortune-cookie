ac400a5accb8acf2cd6b442483240a6e29690f7f
kayhui46 <365kaehui@nhusd.k12.ca.us>
2015-12-17 23:48:52 +0000

    adding missing files

--- index.js
+++ index.js
@@ -4,19 +4,9 @@
           $.getJSON("fortunes.json", function(json) {
              console.log("success" + JSON.stringify(json));
              for(var emotion in json) {
-                 console.log(json.open[emotion].length)
+                 console.log(json.open[emotions].length)
              }
           });
-          //.done(function(json) {
-          //   console.log("second success" + JSON.stringify(json));
-          //})
-          //.fail(function(e) {
-          //    console.log("error: " + JSON.stringify(e));
-          //});
-           
-          //jqxhr.complete(function() {
-          //    console.log("second complete");
-          //});
      });
  }
  
