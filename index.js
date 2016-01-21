11a56c7748c25c3d105c02bf81205a10a97f7b81
kayhui46 <365kaehui@nhusd.k12.ca.us>
2016-01-21 23:55:36 +0000

    emotionsWithJSON

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
  function loadFortunes() {
      $(document).ready(function() {
 
-          $.getJSON("fortunes.json", function(json) {
+         $.getJSON("fortunes.json", function(json) {
              console.log("success" + JSON.stringify(json));
-             for(var emotion in json) {
-                 console.log(json.open[emotions].length)
+             for (var emotion in json) {
+                 console.log(json[emotion].length);
              }
-          });
+             printFortunes(json);
+         });
+        
+
      });
  }
- 
- for (prop in json)
-    console.log(prop+"\n");
-    $("ul.emotions").append("<li>"+prop+"</li>");
 
-_.each(json["mean"], function(fortune,i) {
-    console.log(i+""+fortune);
-});
+function printFortunes(json) {
+var emotionName = $("ul.emotions");
+ for (var prop in json) {
+     console.log(prop + "\n");
+     var el = $("<li>" + prop + "</li>");
+    el.appendTo(emotionName); 
+ }
+ for (var emotion in json) {
+        json[emotion].forEach(function(fortune, i) {
+        console.log(i + "" + fortune);
+    }
+ );
  
+ 
+}}
